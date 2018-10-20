5a9290c147cd20f50f535a1ef796b064c2789615
Scott Schwartz <scott@friendbuy.com>
2018-10-20 11:24:02 -0700

    clean up create react app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import "./styles/index.scss";
-// import { hot } from "react-hot-loader";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -14,9 +11,3 @@ if (module.hot && process.env.NODE_ENV === "development") {
     ReactDOM.render(<NextApp />, document.getElementById("root"));
   });
 }
-// export default hot(module)(Root);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
