87ef7df4480d62e902de14862de27da4d9214924
Scott Schwartz <scott@friendbuy.com>
2018-10-20 22:08:46 -0700

    updates

---
+++ src/components/NewCertificateDetails.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { ContentHeader, ButtonSmall, ProgressBar } from "./";
+
+export class NewCertificateDetails extends React.PureComponent {
+  state = {
+    isOpen: false,
+  };
+
+  onSelect = () => {
+    this.props.history.push("/user/saver/certificates/1");
+  };
+
+  toggleDetails = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    const {
+      completed,
+      total,
+      termLength,
+      interestRate,
+      title,
+      description,
+    } = this.props;
+
+    return (
+      <div
+        className="new-certificate-details-container"
+        onClick={this.toggleDetails}
+      >
+        {/* {isOpen && (
+          <div className="new-certificate-details-card-header">
+            <ContentHeader title={title} />
+          </div>
+        )} */}
+        <div className="new-certificate-progress-bar">
+          <ProgressBar title={title} completed={completed} total={total} />
+        </div>
+        {isOpen && (
+          <>
+            <div className="new-certificate-term-details">
+              ${termLength} months at {interestRate}%
+            </div>
+            <div className="new-certificate-term-description">
+              {description}
+            </div>
+            <div className="new-certificate-details-button">
+              <ButtonSmall text="Select" onClick={this.onSelect} />
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+export const NewCertificateDetailsWrapped = withRouter(NewCertificateDetails);
--- src/modules/saver/RouteContainer.jsx
+++ src/modules/saver/RouteContainer.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Route, withRouter } from "react-router-dom";
 import { DashboardContainerWrapped as Dashboard } from "./Dashboard/Container";
+import { CertificatesContainerWrapped as Certificates } from "./Certificates/Container";
+import { NewCertificate } from "./Certificates/NewCertificate";
 
 export class SaverContainer extends React.PureComponent {
   render() {
     return (
       <div className="saver-container">
         <Route path="/user/saver" exact={true} component={Dashboard} />
+        <Route
+          path="/user/saver/certificates"
+          exact={true}
+          component={Certificates}
+        />
+        <Route
+          path="/user/saver/certificates/:id"
+          exact={true}
+          component={NewCertificate}
+        />
       </div>
     );
   }
